b0a78c48e5c434720f0916b0b205479f7d6449f1
mathetes041 <matthewonuoha41@gmail.com>
2024-04-02 17:20:44 +0100

    SEO fixes

--- src/components/nav.jsx
+++ src/components/nav.jsx
@@ -4,7 +4,7 @@ const Nav = () => {
   return (
     <div className="nav">
       <div className="logo">
-        <h1>PhotoGallery</h1>
+        <h1>MathetesPhotoGallery</h1>
       </div>
       <a
         href="https://matthew-vincent.netlify.app"
